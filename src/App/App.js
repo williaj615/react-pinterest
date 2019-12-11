695d8a335e4946deafe971fc2d35e1519aa2e021
Jasmine Williams <jasminewilliams2017@gmail.com>
2019-12-10 21:35:40 -0600

    can log in

--- src/App/App.js
+++ src/App/App.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
+import firebase from 'firebase/app';
 import './App.scss';
+import firebaseConnection from '../helpers/data/connection';
+import Auth from '../components/Auth/Auth';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <button className="btn btn-info">REACT PINTEREST</button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+firebaseConnection.firebaseApp();
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+
+    return (
+      <div className="App">
+          <button className="btn btn-info">REACT PINTEREST</button>
+          {
+            (authed) ? (<div>You logged in</div>) : (<Auth />)
+          }
+      </div>
+    );
+  }
 }
 
 export default App;
